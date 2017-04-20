921f6ed6ebc89f405dd28db5b33ae5338de0786d
fenilpatel35 <fenilpatel35@gmail.com>
2017-04-19 19:34:08 -0500

    Add files via upload

---
+++ Parse-ConfigFiles.ps1
@@ -0,0 +1,203 @@
+﻿class Database {
+    [string] $Key;
+    [string] $Server;
+    [string] $Database;
+
+    Database(){}
+
+    Database([string]$key, [string] $server, [string] $database){
+        $this.Key = $key;
+        $this.Server = $server;
+        $this.Database = $database;
+    }
+}
+
+class AppConnection {
+    [string] $Key;
+    [string] $Connection;
+
+    AppConnection(){}
+
+    AppConnection([string]$key, [string]$connection){
+        $this.Key = $key;
+        $this.Connection = $connection;
+    }
+}
+
+class Inventory {
+    [string] $Name;
+    [Database[]] $DBConnections;
+    [AppConnection[]] $AppConnections;
+    
+    Inventory([string]$name){
+        $this.Name = $name;
+        [Database]::new();
+        [AppConnection]::new();
+    }
+
+    Inventory ([Database[]]$dbs,[AppConnection[]]$endpoints) {
+        $this.DBConnections = $dbs;
+        $this.AppConnections = $endpoints;
+    }
+
+
+
+    [void] AddDb([Database]$db){
+        $this.DBConnections += $db
+    }
+
+    [void] AddApp([AppConnection]$endpoint){
+        $this.AppConnections += $endpoint
+    }
+}
+
+
+function Parse-ConfigFiles {
+    param([string]$FolderPath = "C:\Users\Fenil\Downloads\Configs\RTGE Edits")
+
+    Begin {
+
+    }
+
+    Process {
+        $configs = Get-ChildItem -Path $FolderPath -Filter "*.config"
+
+        $inventory = [Inventory]::new($FolderPath);
+
+        foreach ($config in $configs) {
+            $filePath = $config.FullName
+            $filePath
+
+            $c = [xml](Get-Content $filePath)
+
+            
+            foreach($setting in $c.appSettings.add){
+                if($setting.value -match "Server="){
+                    $key = $setting.key
+                    $server = $setting.value.split(";") -match "Server=" | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Server"}
+                    $database = $setting.value.split(";") -match "Database="  | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Database"}
+                    $inventory.AddDb([Database]::new($key,$server,$database))
+                }
+
+                if($setting.value -match "Source="){
+                    $key = $setting.key
+                    $server = $setting.value.split(";") -match "Data Source=" | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Data Source"}
+                    $database = $setting.value.split(";") -match "Initial Catalog="  | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Initial Catalog"}
+                    $inventory.AddDb([Database]::new($key,$server,$database))
+                }
+
+                if(($setting.value -match 'http:') -or ($setting.value -match 'https:') -or ($settings.value -match 'tcp:') -or ($settings.value -match '\\\\\w+')){
+                    $key = $setting.key
+                    $conn = $setting.value
+                    $inventory.AddApp([AppConnection]::new($key,$conn))
+                }
+            }
+            
+
+            foreach($connString in $c.connectionStrings.add){
+                if($connString.connectionString -match "Server="){
+                    $key = $connString.name
+                    $server = $connString.connectionString.split(";") -match "Server=" | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Server"}
+                    $database = $connString.connectionString.split(";") -match "Database="  | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Database"}
+                    $inventory.AddDb([Database]::new($key,$server,$database))
+                }
+
+                if($connString.connectionString -match "Source="){
+                    $key = $connString.name
+                    $server = $connString.connectionString.split(";") -match "Data Source=" | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Data Source"}
+                    $database = $connString.connectionString.split(";") -match "Initial Catalog="  | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Initial Catalog"}
+                    $inventory.AddDb([Database]::new($key,$server,$database))
+                }
+
+                if(($connString.connectionString -match 'http:') -or ($connString.connectionString -match 'https:') -or ($connString.connectionString -match 'tcp:')){
+                    $key = $connString.name
+                    $conn = $connString.connectionString
+                    $inventory.AddApp([AppConnection]::new($key,$conn))
+                }
+            }
+            
+
+            foreach($client in $c.client.endpoint){
+                $key = $client.contract
+                $endpoint = $client.address
+                $inventory.AddApp([AppConnection]::new($key,$endpoint));
+            }
+
+            foreach($ems in $c.EmsConnections.connections.add){
+                $key = $ems.name
+                $server = $ems.server
+                $inventory.AddApp([AppConnection]::new($key,$server));
+            }
+
+            foreach($setting in $c.configuration.appSettings.add){
+                if($setting.value -match "Server="){
+                    $key = $setting.key
+                    $server = $setting.value.split(";") -match "Server=" | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Server"}
+                    $database = $setting.value.split(";") -match "Database="  | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Database"}
+                    $inventory.AddDb([Database]::new($key,$server,$database))
+                }
+
+                if($setting.value -match "Source="){
+                    $key = $setting.key
+                    $server = $setting.value.split(";") -match "Data Source=" | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Data Source"}
+                    $database = $setting.value.split(";") -match "Initial Catalog="  | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Initial Catalog"}
+                    $inventory.AddDb([Database]::new($key,$server,$database))
+                }
+
+                if(($setting.value -match 'http:') -or ($setting.value -match 'https:') -or ($settings.value -match 'tcp:') -or ($settings.value -match '\\\\\w+')){
+                    $key = $setting.key
+                    $conn = $setting.value
+                    $inventory.AddApp([AppConnection]::new($key,$conn))
+                }
+            }
+            
+
+            foreach($connString in $c.configuration.connectionStrings.add){
+                if($connString.connectionString -match "Server="){
+                    $key = $connString.name
+                    $server = $connString.connectionString.split(";") -match "Server=" | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Server"}
+                    $database = $connString.connectionString.split(";") -match "Database="  | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Database"}
+                    $inventory.AddDb([Database]::new($key,$server,$database))
+                }
+
+                if($connString.connectionString -match "Source="){
+                    $key = $connString.name
+                    $server = $connString.connectionString.split(";") -match "Data Source=" | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Data Source"}
+                    $database = $connString.connectionString.split(";") -match "Initial Catalog="  | ForEach-Object {$_ -split "="} | Where-Object {$_ -ne "Initial Catalog"}
+                    $inventory.AddDb([Database]::new($key,$server,$database))
+                }
+
+                if(($connString.connectionString -match 'http:') -or ($connString.connectionString -match 'https:') -or ($connString.connectionString -match 'tcp:')){
+                    $key = $connString.name
+                    $conn = $connString.connectionString
+                    $inventory.AddApp([AppConnection]::new($key,$conn))
+                }
+            }
+            
+
+            foreach($client in $c.configuration.system.serviceModel.client.endpoint){
+                $key = $client.contract
+                $endpoint = $client.address
+                $inventory.AddApp([AppConnection]::new($key,$endpoint));
+            }
+
+            foreach($ems in $c.configuration.EmsConnections.connections.add){
+                $key = $ems.name
+                $server = $ems.server
+                $inventory.AddApp([AppConnection]::new($key,$server));
+            }
+             
+        }
+
+        #$inventory | Format-Table
+        $inventory | ConvertTo-Json
+
+    }
+
+    End {
+
+    }
+
+}
+
+Parse-ConfigFiles
---
+++ Parse-Configs.ps1
@@ -0,0 +1,68 @@
+﻿class Inventory2 {
+    [string] $Server;
+    [string] $FileName;
+    [string[]] $Connections;
+
+    Inventory2([string]$server, [string]$fileName){
+        $this.Server = $server;
+        $this.FileName = $fileName;
+        $this.Connections = @()
+    }
+
+    
+    Inventory2([string]$server, [string]$fileName, [string[]]$conn){
+        $this.Server = $server;
+        $this.FileName = $fileName;
+        $this.Connections = $conn;
+    }
+
+    [void] Add([string]$value){
+        $this.Connections += $value
+    }
+}
+
+$FolderPath = "C:\users\Fenil\Downloads\Configs\MES\Web.config"
+$configs = Get-ChildItem -Path $FolderPath -Recurse -Filter "*.config"
+foreach ($config in $configs){
+$c = (Get-Content $config.FullName)
+
+$connections = @()
+$connections += ($c -match "\\\\\w+");
+$connections += ($c -match "Server=");
+$connections += ($c -match "Data Source=");
+$connections += ($c -match "http:");
+$connections += ($c -match "https:");
+$connectionsupdated = @()
+foreach($val in $connections){
+    try{
+        $xmlVal = [xml]$val
+        if($xmlVal.add.Value){$connectionsupdated += $xmlVal.add.value}
+        if($xmlVal.add.server){$connectionsupdated += $xmlVal.add.server}
+        if($xmlVal.add.connectionString){$connectionsupdated += $xmlVal.add.connectionString}
+        if($xmlVal.endpoint){$connectionsupdated += $xmlVal.endpoint.address}
+        if($xmlVal.sessionState){$connectionsupdated += $xmlVal.sessionState.sqlConnectionString}
+        if($xmlVal.value){$connectionsupdated += $xmlVal.value}
+    }
+    catch{
+        $val += " />"
+        $xmlVal = [xml]$val
+        if($xmlVal.add.Value){$connectionsupdated += $xmlVal.add.value}
+        if($xmlVal.add.server){$connectionsupdated += $xmlVal.add.server}
+        if($xmlVal.add.connectionString){$connectionsupdated += $xmlVal.add.connectionString}
+        if($xmlVal.endpoint){$connectionsupdated += $xmlVal.endpoint.address}
+        if($xmlVal.sessionState){$connectionsupdated += $xmlVal.sessionState.sqlConnectionString}
+        if($xmlVal.value){$connectionsupdated += $xmlVal.value}
+    }
+    
+}
+
+$config.FullName
+Write-Host "diff btw old : updated -- $($connections.Count) : $($connectionsupdated.Count)"
+$connections
+Write-Host "items in updated $($connectionsupdated.Count)"
+$connectionsupdated
+
+$inventory2 = [Inventory2]::new($env:ComputerName, $config.FullName,$connectionsupdated)
+
+
+}
